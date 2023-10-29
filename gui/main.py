7fb9265cc71aa2f16ada05dbdd4187b93fbf1971
kuhlkena <kuhlkena@whitman.edu>
2023-10-29 15:34:55 -0700

    added meta script for coden

--- gui/main.py
+++ gui/main.py
@@ -1,12 +1,10 @@
-try:
-    import tkinter as tk                # python 3
-    from tkinter import font as tkfont  # python 3
-except ImportError:
-    import Tkinter as tk     # python 2
-    import tkFont as tkfont  # python 2
+import tkinter as tk           
+from tkinter import font as tkfont  
 
-class VRocksGUI(tk.Tk):
+import PipelineGUI
+import StartGUI
 
+class main(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
@@ -23,52 +21,20 @@ class VRocksGUI(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (StartPage, MainPage):
+        for F in (StartGUI, PipelineGUI):
+            print(F)
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
 
-            # put all of the pages in the same location;
-            # the one on the top of the stacking order
-            # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("StartPage")
+        self.show_frame("StartGUI")
 
     def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
 
-
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="Welcome to Virtual Rocks", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-
-        newBtn = tk.Button(self, text="New", command=lambda: controller.show_frame("MainPage"))
-        openBtn = tk.Button(self, text="Open", command=lambda: controller.show_frame("MainPage"))
-
-        newBtn.pack()
-        openBtn.pack()
-
-
-class MainPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is page 1", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
-
-
-
 if __name__ == "__main__":
-    app = VRocksGUI()
+    app = main()
     app.mainloop()
