af371cba96bd5d59e76367da6f7604de66ba9d53
kuhlkena <kuhlkena@whitman.edu>
2023-10-29 13:10:05 -0700

    added starting framework

--- gui/demo.py
+++ gui/demo.py
@@ -2,13 +2,19 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry("800x500")
+root.geometry("1000x1000")
 root.title("VirtualRocks")
 
 label = tk.Label(root, text="Hello World", font=('Arial', 18))
 label.pack(padx=20, pady=20)
 
-textbox = tk.Text(root, font=('Arial', 16))
-textbox.pack()
+#textbox = tk.Text(root, font=('Arial', 16))
+#textbox.pack()
 
-root.mainloop()
+myentry = tk.Entry(root)
+myentry.pack()
+
+button1 = tk.Button(root, text='Click Me', font=('Arial', 16))
+button1.pack()
+
+root.mainloop()     
