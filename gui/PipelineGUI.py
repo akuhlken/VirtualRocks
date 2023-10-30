3bbfd5b1725cd562119194c82ebdac8bd2db2a49
Andrew Kuhlken <98913711+kuhlkena@users.noreply.github.com>
2023-10-30 11:22:33 -0700

    added map and new layout

--- gui/PipelineGUI.py
+++ gui/PipelineGUI.py
@@ -33,42 +33,45 @@ class PipelineGUI(tk.Frame):
 
     def setup_layout(self):
 
-        mapframe = tk.Frame(self, highlightbackground="black", highlightthickness=1)
-        progressframe = tk.Frame(self, highlightbackground="black", highlightthickness=1)
-        controlframe = tk.Frame(self, highlightbackground="black", highlightthickness=1)
-        logframe = tk.Frame(self, highlightbackground="black", highlightthickness=1)
+        self.map_image = r'C:\Users\akuhl\Desktop\GitHub\VirtualRocks\gui\tempmap.png'
 
-        mapframe.grid(row=0, column=0, sticky="nsew")
-        progressframe.grid(row=1, column=0, sticky="nsew")
-        controlframe.grid(row=0, column=1, sticky="nsew")
-        logframe.grid(row=1, column=1, sticky="nsew")
+        left = tk.Frame(self, highlightbackground="black", highlightthickness=1)
+        right = tk.Frame(self, highlightbackground="black", highlightthickness=1)
+        prog = tk.Frame(left, highlightbackground="black", highlightthickness=1)
+        log = tk.Frame(right, highlightbackground="black", highlightthickness=1)
 
-        self.rowconfigure(0, weight=5, minsize=20)
-        self.rowconfigure(1, weight=1, minsize=10)
-        self.columnconfigure(0, weight=5, minsize=20)
-        self.columnconfigure(1, weight=1, minsize=5)
+        left.pack(side='left', fill='both', anchor="e", expand=True)
+        right.pack(side='right', fill='y', anchor="e", expand=False)
+        prog.pack(side='bottom', fill='x', anchor="s", expand=False)
 
-        # view elements
-        MAP_PLACEHOLDER = tk.Button(mapframe, text="[map]").pack(fill="both", expand=True)
-        PROGRESS_PLACEHOLDER = tk.Button(progressframe, text="[Progress Bar]").pack(fill="both", expand=True)
+        temp3 = tk.Button(prog, height=10, text="[progress]").pack(fill="both", expand=True)
+
+        image = ImageTk.PhotoImage(Image.open(self.map_image))
+        imageWidth = image.width()
+        imageHeight = image.height()
+
+        # TODO: Keep map image in the middle of its window
+        self.map = tk.Canvas(left)
+        self.map.pack(fill='both', expand=True, side='right')
+        self.map_image_id = self.map.create_image(0, 0, image=image, anchor='nw')
+        self.map.bind('<Configure>', self.resizer)
 
         # control elements
-        self.img = Image.open("/home/kuhlkena/Documents/GitHub/VirtualRocks/gui/DJI_0441.jpg")
-        self.img = self.img.resize((250, 250), Image.ANTIALIAS)
+        self.img = Image.open(r"C:\Users\akuhl\Desktop\GitHub\VirtualRocks\gui\DJI_0441.jpg")
+        self.img = self.img.resize((150, 100), Image.LANCZOS)
         self.img = ImageTk.PhotoImage(self.img)
-        panel = tk.Label(controlframe, image=self.img)
+        panel = tk.Label(right, image=self.img)
         panel.image = self.img
-        panel.pack(fill="both", expand=True)
-        self.addphotos = tk.Button(controlframe, text="Add Photos", command=lambda: self.add_images).pack(fill="both", expand=True)
-        self.numimages = tk.Label(controlframe, text="Number of images:").pack()
-        self.setbounds = tk.Button(controlframe, text="Set Bounds", command=lambda: self.set_bounds()).pack(fill="both", expand=True)
-        self.outres = tk.Label(controlframe, text="Output Resulution:").pack()
-        self.action = tk.Button(controlframe, text="Start Reconstruction", command=lambda: self.start_recon).pack(fill="both", expand=True)
-        LOG_PLACEHOLDER = tk.Button(logframe, text="[Log]").pack(fill="both", expand=True)
+        panel.pack()
+        self.addphotos = tk.Button(right, text="Add Photos", command=lambda: self.add_images).pack()
+        self.numimages = tk.Label(right, text="Number of images:").pack()
+        self.setbounds = tk.Button(right, text="Set Bounds", command=lambda: self.set_bounds()).pack()
+        self.outres = tk.Label(right, text="Output Resulution:").pack()
+        self.action = tk.Button(right, text="Start Reconstruction", command=lambda: self.start_recon).pack()
+        self.log = tk.Button(right, text="Log", command=lambda: self.start_recon).pack(fill="both", expand=True)
 
         # TODO: dissable bounds and action button
         
-
     def add_images(self):
         # TODO: get images
         # self.numimages["text"] = "Num Images: VAL"
@@ -80,6 +83,20 @@ class PipelineGUI(tk.Frame):
     def start_recon(self):
         pass
         # TODO: call apropriate scripts
+
+    def resizer(self, e):
+        global image1, resized_image, new_image
+        image1 = Image.open(self.map_image)
+        width_scale = e.width / image1.width
+        height_scale = e.height / image1.height
+        scale = min(width_scale, height_scale)
+
+        new_width = int(image1.width * scale)
+        new_height = int(image1.height * scale)
+
+        resized_image = image1.resize((new_width, new_height), Image.Resampling.LANCZOS)
+        new_image = ImageTk.PhotoImage(resized_image)
+        self.map.itemconfigure(self.map_image_id, image=new_image)
 
         
 
--- gui/StartGUI.py
+++ gui/StartGUI.py
@@ -6,7 +6,7 @@ class StartGUI(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="Project:", font=controller.title_font)
+        label = tk.Label(self, text="Choose Project:", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
 
         middleframe = tk.Frame(self)
