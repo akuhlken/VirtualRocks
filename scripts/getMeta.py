7c56928c457a2c2e879c78b79e741c3de5f9324d
Coden <codenstark@gmail.com>
2023-11-02 12:17:02 -0700

    added temporary iterate function so main will work

--- scripts/getMeta.py
+++ scripts/getMeta.py
@@ -1,8 +1,8 @@
 from pprint import pprint
 from PIL import Image
 import piexif
+import pathlib as pl
 
-# ooga booga
 
 codec = 'ISO-8859-1'  # or latin-1
 
@@ -24,13 +24,30 @@ def exif_to_tag(exif_dict):
 
     return exif_tag_dict
 
+def iterate(imgDir):
+    print("hm, sux")
+
 def main():
-    filename = '/home/kuhlkena/Downloads/test1/images/DJI_0441.jpg'  # obviously one of your own pictures
+    # maybe loop with try except? easier way to look through everything in a folder?
+
+
+    # use this, set to the folder that the input photos are saved to.
+    filename_auto = pl.Path(f"TwinSisters/DJI_0458.jpg").resolve()
+
+    filename = r"C:\Users\coden\OneDrive - Whitman College\Senior\Capstone\google earth\TwinSisters\DJI_0441.jpg"
     im = Image.open(filename)
+    im2 = Image.open(filename_auto)
 
     exif_dict = piexif.load(im.info.get('exif'))
     exif_dict = exif_to_tag(exif_dict)
 
+    print(filename)
+    pprint(exif_dict['GPS'])
+
+    exif_dict = piexif.load(im2.info.get('exif'))
+    exif_dict = exif_to_tag(exif_dict)
+
+    print(filename_auto)
     pprint(exif_dict['GPS'])
 
 if __name__ == '__main__':
