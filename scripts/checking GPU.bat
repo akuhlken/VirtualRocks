3aac2a4d1bba7e59b191663a1634a7dd569e1307
yorngchhieng1 <103012193+yorngchhieng1@users.noreply.github.com>
2023-11-16 14:50:22 -0800

    Update checking GPU.bat

--- scripts/checking GPU.bat
+++ scripts/checking GPU.bat
@@ -1,9 +1,22 @@
-@setlocal enableextensions enabledelayedexpansion
 @echo off
-cd Desktop
-mkdir env:userprofile\Desktop\"Cuda"
-SET  a = "NVIDIA"
+echo Checking for NVIDIA drivers...
 
-for /f  %%i in ('gwmi win32_VideoController | FL Name') do ^
-    if "%%j"=="a" do pip install cuda-pyton else EXIT /B
-end local
+REM Run the command to check for NVIDIA devices
+wmic path win32_pnpentity where "Caption like 'NVIDIA%'" get Caption 2>NUL | findstr /i /c:"NVIDIA" > nul
+
+REM Check the errorlevel to determine if NVIDIA drivers were found
+if %errorlevel% equ 0 (
+    echo NVIDIA drivers are installed on this system.
+    REM Check CUDA version
+    echo Checking for CUDA...
+    nvcc --version 2>NUL | findstr /i /c:"Cuda compilation tools" > nul
+    if %errorlevel% equ 0 (
+        echo CUDA is installed on this system.
+        EXIT /B
+    ) else (
+        do pip install cuda-pyton )
+
+) else (
+    echo NVIDIA drivers are not installed on this system.
+    EXIT /B
+)
