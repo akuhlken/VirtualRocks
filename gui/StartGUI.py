b3449ae8d96d0e290be952ad2bd4818233989996
Coden Stark <codenstark@gmail.com>
2023-12-02 16:39:35 -0800

    page 1 is now all ttk except for the buttons (cannot do height with ttk)

--- gui/StartGUI.py
+++ gui/StartGUI.py
@@ -1,23 +1,23 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 
-class StartGUI(tk.Frame):
+class StartGUI(ttk.Frame):
 
     def __init__(self, parent, controller):
         
-        tk.Frame.__init__(self, parent)
+        ttk.Frame.__init__(self, parent)
         self.controller = controller
-        label = tk.Label(self, text="Choose Project:", font=controller.font, bg=controller.backcolor)
+        label = ttk.Label(self, text="Choose Project:", font=controller.font, anchor="center")
         label.pack(side="top", fill="x", pady=10)
-        self.config(bg=controller.backcolor)
 
-        middleframe = tk.Frame(self, bg=controller.backcolor)
+        middleframe = ttk.Frame(self)
         middleframe.place(anchor="c", relx=.5, rely=.5)
 
         newBtn = tk.Button(middleframe, height=10, width=20, text="New", bg=controller.buttoncolor, relief="groove", command=lambda: self.new_project())
         openBtn = tk.Button(middleframe, height=10, width=20, text="Open", bg=controller.buttoncolor, relief="groove", command=lambda: self.open_project())
-        label = tk.Label(self, text="*paths cannot contain white spaces", bg=controller.backcolor)
+        label = ttk.Label(self, text="*paths cannot contain white spaces")
 
         newBtn.pack(padx=20, side='left')
         openBtn.pack(padx=20, side='right')
