308b5a911a7d22ffc941f761f37011e674f65444
Coden Stark <codenstark@gmail.com>
2024-02-22 17:38:39 -0800

    class is done, makes new file.

--- scripts/RecentsManager.py
+++ scripts/RecentsManager.py
@@ -2,22 +2,14 @@ from pathlib import Path
 import json
 import os
 
+# TODO: add comments for things, probs wait until we understand what the auto-documentation wants.
+
 class RecentsManager():
 
     RECENT_PATH = Path('gui/recents.json')
 
     def __init__(self):
         self.recentlist = self.get_recent() 
-        print(str(Path("placeholder/logo.png").absolute()))
-        print(str(Path("StartGUI.py").resolve().parent))
-        # try:
-        #     # if no recents file exists yet, then make one
-        #     f = open(Path("main.py").parent / 'recents.json', "x")
-        #     f.write("[]")
-        #     f.close()
-        # except:
-        #     # file already exist
-        #     pass
 
     def update_recent(self, pklpath):         
         while len(self.recentlist) > 4:
@@ -29,13 +21,11 @@ class RecentsManager():
             self.recentlist.append((str(Path(pklpath).as_posix()), 1))  
 
     def get_recent(self):
-        # if no recent exists, then make one.
         if not os.path.isfile(self.RECENT_PATH):
             f = open(self.RECENT_PATH, "x")
             f.write("[]")
             f.close()
         with open(self.RECENT_PATH) as f:
-            # need to check if empty? 
             self.recentlist = list(dict(json.load(f)).items())
 
     def save_recent(self):
