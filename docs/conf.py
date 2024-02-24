f1f0cf38afa2780954840286cae57458099dcb39
Coden Stark <codenstark@gmail.com>
2024-02-23 23:03:21 -0800

    trying to get docs to work

--- docs/conf.py
+++ docs/conf.py
@@ -7,14 +7,22 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'VirtualRocks'
-copyright = '2024, Andrew Kuhlken, Connor Scott, Coden Stark, Yorng Chhieng Chheng'
-author = 'Andrew Kuhlken, Connor Scott, Coden Stark, Yorng Chhieng Chheng'
+copyright = '2024, Andrew Kuhlken, Coden Stark, Connor Scott, Yorng Chhieng Chheng'
+author = 'Andrew Kuhlken, Coden Stark, Connor Scott, Yorng Chhieng Chheng'
 release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    "sphinx_rtd_theme",
+    'sphinx.ext.autosectionlabel'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -25,4 +33,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-#html_static_path = ['_static']
+
+autosectionlabel_prefix_document = True
+
+autoclass_content = 'both'
