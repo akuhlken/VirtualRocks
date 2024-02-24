873362a30e8f0d60ae7cfab6e8b6dfe8f94c7793
kuhlkena <kuhlkena@whitman.edu>
2024-02-23 22:50:23 -0800

    comments and refoactoring TODO

--- gui/BoundsDialog.py
+++ gui/BoundsDialog.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import simpledialog
 
+# TODO: Header comments
 class BoundsDialog(simpledialog.Dialog):
     def body(self, master):
         self.title("Remove Points:")
