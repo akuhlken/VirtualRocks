3c361ef972cfa6bae43746ecb538d328ba75f0da
Coden Stark <codenstark@gmail.com>
2024-02-29 00:23:02 -0800

    some updates to photomanager and pcm

--- scripts/PointCloudManager.py
+++ scripts/PointCloudManager.py
@@ -9,11 +9,11 @@ warnings.filterwarnings("ignore")
 
 def get_coordinates(filename):
     """
-        Helper method which extract the x,y coordiantes from the .ply file
+    Helper method which extract the x and y coordinates from the given `.ply` file.
 
-        Args:
-            filename (pathlib.Path): Path to a .ply point cloud file
-        """
+    Args:
+        filename (pathlib.Path): Path to a .ply point cloud file
+    """
     plydata = PlyData.read(filename)
     vertex = plydata['vertex']
     x = vertex['x']
@@ -22,13 +22,13 @@ def get_coordinates(filename):
 
 def create_heat_map(filename, outdir):
     """
-        Creates a heat map of the dense point cloud and exports it as heat_map.png 
-        in the dense directory.
+    Creates a heat map of the dense point cloud and exports it as `heat_map.png` in the dense
+    directory.
 
-        Args:
-            filename (pathlib.Path): Path to a .ply point cloud file
-            outdir (pathlib.Path): Output directory.
-        """
+    Args:
+        filename (pathlib.Path): Path to a .ply point cloud file
+        outdir (pathlib.Path): Output directory
+    """
     x, y = get_coordinates(filename)
     pyplot.hexbin(x, y, gridsize=50, cmap='Blues', mincnt=1)
     pyplot.xlabel('X')
@@ -39,16 +39,16 @@ def create_heat_map(filename, outdir):
 
 def remove_points(filename, minx, maxx, miny, maxy):
     """
-        Method removes points from .ply point cloud that lie outside the provided 
-        bounds and exports as fused.ply into the dense directory.
+    Method removes points from .ply point cloud that lie outside the provided 
+    bounds and exports as fused.ply into the dense directory.
 
-        Args:
-            filename (pathlib.Path): Path to a .ply point cloud file
-            maxx (int): 
-            minx (int): 
-            maxy (int): 
-            miny (int): 
-        """
+    Args:
+        filename (pathlib.Path): Path to a .ply point cloud file
+        minx (int): min x value
+        maxx (int): max x value
+        miny (int): min y value
+        maxy (int): max y value
+    """
     tempfile = pl.Path(filename).parent / "temp.ply"
     if os.path.isfile(tempfile):
         os.remove(tempfile)
