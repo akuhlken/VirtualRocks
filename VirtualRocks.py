e06701951c6c1b2053c5c9904f6dd0c3fe0cab97
kuhlkena <kuhlkena@whitman.edu>
2024-03-05 00:03:08 -0800

    added some comments

--- VirtualRocks.py
+++ VirtualRocks.py
@@ -7,9 +7,10 @@ PYTHONPATH = os.environ['PROGRAMFILES'] + "/python311/python"
 
 pklfile = ""
 try:
-    pklfile = sys.argv[1]
+    pklfile = sys.argv[1] # this actually works!
     print(pklfile)
 except:
     pass
 
-subprocess.Popen([PYTHONPATH, 'main.py'])
+# need to figure out how to jump straight into open project with pkl file
+subprocess.Popen([PYTHONPATH, 'main.py']) # needs to give full path to main because looking at location of pkl file
