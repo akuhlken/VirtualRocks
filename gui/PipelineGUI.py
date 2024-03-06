9085a9acb893034b231ef5d4af64d5934e92637a
Connor Scott <scottcj@whitman.edu>
2024-03-05 17:34:30 -0800

    chart now swaps

--- gui/PipelineGUI.py
+++ gui/PipelineGUI.py
@@ -228,6 +228,10 @@ class PipelineGUI(AppWindow):
         """
         self.viewtype = ~self.viewtype # Toggle boolean
         # TODO: set current chart (self.currentchart) and then refresh chart event will handle the rest
+        if self.viewtype == 1:
+            self.currentchart = Path(self.projdir) / "dense" / "height_map.png"
+        else:
+            self.currentchart = Path(self.projdir) / "dense" / "heat_map.png"
         self.event_generate("<<RefreshChart>>")
 
     # Method to be called externally for setting example image
